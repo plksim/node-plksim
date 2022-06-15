23373209b08c832f7a022c6b4f2856bf6fbfd96f
vvlookman <vvlookman@gmail.com>
2022-06-15 18:17:04 +0800

    Add a sample method wrapper

--- src/binding.cc
+++ src/binding.cc
@@ -1,4 +1,6 @@
 #include <napi.h>
+
+#include <plksim/samples.hh>
 #include <plksim/version.hh>
 
 using namespace Napi;
@@ -9,10 +11,17 @@ Value NativeVersionGetter(const CallbackInfo& info) {
   return String::New(info.Env(), VERSION);
 }
 
+String SampleMeshSvgMethod(const Napi::CallbackInfo& info) {
+  auto svg = sampleMeshSvg();
+  return String::New(info.Env(), svg);
+}
+
 Object Init(Env env, Object exports) {
   PropertyDescriptor nativeVersion = PropertyDescriptor::Accessor<NativeVersionGetter>("nativeVersion");
-
   exports.DefineProperties({nativeVersion});
+
+  exports.Set(String::New(env, "sampleMeshSvg"), //
+              Function::New(env, SampleMeshSvgMethod));
 
   return exports;
 }
--- test/binding.test.js
+++ test/binding.test.js
@@ -3,3 +3,8 @@ const plksim = require("../lib/binding.js");
 test('properties', () => {
   expect(plksim.nativeVersion).toMatch(/^[0-9]*\.[0-9]*\.[0-9]*$/);
 });
+
+test('samples', () => {
+  const svg = plksim.sampleMeshSvg();
+  console.log(svg);
+});
